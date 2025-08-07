0cba8eb6f71c53a21a0b57921e359fdf68e30dc7
Ritesh Pokale <riteshspoklae27@gmail.com>
2025-08-07 13:01:55 +0530

    Add Terraform files for Docker container task

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "docker_image" "nginx_image" {
 
 resource "docker_container" "nginx_container" {
   name  = "nginx-container"
-  image = docker_image.nginx_image.latest
+  image = docker_image.nginx_image.name
   ports {
     internal = 80
     external = 8081
